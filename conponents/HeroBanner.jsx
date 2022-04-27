67dd838fd3ddef5ef1da728d3a966b3cd6b2b33f
Akhilesh1902 <akhil1922002@gmail.com>
2022-04-27 20:17:02 +0530

    fixed minor shop button

--- conponents/HeroBanner.jsx
+++ conponents/HeroBanner.jsx
@@ -16,7 +16,7 @@ const HeroBanner = ({ heroBanner }) => {
           className="hero-banner-image"
         />
         <div>
-          <Link href={`products/${heroBanner.product}`}>
+          <Link href={`product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
